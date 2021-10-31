02470841b7cfde522560ac7dcdedb6ecdcb7a901
MohamedFarag <mohamedxiv@gmail.com>
2021-10-31 05:18:18 +0200

    Beta 2

--- main.py
+++ main.py
@@ -8,57 +8,74 @@ import webbrowser
 from mkepdf import makePDFFromString, makePDFFromFile, makePDFFromTemplate
 from mkeqr import saveQRImages
 from mkezip import make_zip_file
+from io import BytesIO
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-  if request.method == "GET":
-      return render_template("index.html")
-  elif request.method == "POST":
-    url = request.form['url']
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        url = request.form["url"]
+        wiki_table = Helpers.getTable(url, False)
+        Helpers.getHyperlinks(wiki_table)
+        return render_template("index.html", table=Helpers.getHtmlTable(url))
+
+
+@app.route("/loadTable/<string:formUrl>", methods=["POST"])
+def load_table(formUrl):
+    url = request.form[formUrl]
     wiki_table = Helpers.getTable(url, False)
     Helpers.getHyperlinks(wiki_table)
-    return render_template('index.html', table=Helpers.getHtmlTable(url)) 
+    return render_template("index.html", table=Helpers.getHtmlTable(url))
 
-@app.route('/downloadExcel', methods=['GET', 'POST'])
+
+@app.route("/downloadExcel", methods=["GET"])
 def download_excel():
-  if request.method == 'GET':
+    # buffer = BytesIO()
     Helpers.pdHtmlToExcel()
-    return send_file(f'{Helpers.excel_file_path}{Helpers.page_title}.xlsx', as_attachment=True)
+    # send_file(buffer, as_attachment=True, attachment_filename='a_file.txt')
+    return send_file(
+        f"{Helpers.excel_file_path}{Helpers.page_title}.xlsx", as_attachment=True
+    )
 
 
-
-@app.route('/saveToGSheet')
+@app.route("/saveToGSheet")
 def save_to_gsheet():
     saveToGSheet()
     webbrowser.open_new_tab(saveToGSheet())
     return redirect(saveToGSheet())
 
 
-@app.route('/downloadPDF', methods=['GET'])
+@app.route("/downloadCSV", methods=["GET"])
 def download_pdf():
-    makePDFFromFile()
-    return send_file('website.pdf', as_attachment=True)
+    Helpers.pdHtmlToCsv()
+    return send_file(
+        f"{Helpers.csv_file_path}{Helpers.page_title}.csv", as_attachment=True
+    )
 
 
-@app.route('/downloadPDFCovers', methods=['GET'])
-@app.route('/downloadPDFCovers/<int:itr>', methods=['GET'])
+@app.route("/getJson", methods=["GET"])
+def get_json():
+    return Helpers.pdHtmlToJson()
+
+
+@app.route("/downloadPDFCovers", methods=["GET"])
+@app.route("/downloadPDFCovers/<int:itr>", methods=["GET"])
 def download_pdf_covers(itr):
     saveQRImages()
     makePDFFromTemplate(itr)
     make_zip_file()
-    return send_file(f'{Helpers.zip_file_path}covers.zip', as_attachment=True)
-    #"Congrats!, you have downloaded the PDFs"
-
-
+    return send_file(f"{Helpers.zip_file_path}covers.zip", as_attachment=True)
+    # "Congrats!, you have downloaded the PDFs"
 
 
 def scrape_table(url):
-  wiki_table = Helpers.getTable(url, False)
-  Helpers.getHyperlinks(wiki_table)
-  print(Helpers.static_final_links)
+    wiki_table = Helpers.getTable(url, False)
+    Helpers.getHyperlinks(wiki_table)
+    print(Helpers.static_final_links)
 
 
 if __name__ == "__main__":
